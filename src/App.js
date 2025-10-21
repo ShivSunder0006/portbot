93376da1730825dbd8298717206c14c082e04f16
ShivSunder0006 <101868720+ShivSunder0006@users.noreply.github.com>
2025-10-21 22:14:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Projects setSelectedProject={setSelectedProject} />
         <Skills />
         <Contact />
-        <Chatbot />
+        // <Chatbot />
       </main>
       <Footer />
 
