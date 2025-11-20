8e8bff9886a20513e545ba5a57622d3c39fd3b3c
ShivSunder0006 <101868720+ShivSunder0006@users.noreply.github.com>
2025-11-21 01:50:44 +0530

    Remove Chatbot import from App.js
    Comment out Chatbot import to prevent usage.

--- src/App.js
+++ src/App.js
@@ -10,12 +10,6 @@ import Contact from "./pages/Contact";
 import ProjectModal from "./components/ProjectModal";
 import Certifications from "./pages/Certifications";
 
-<<<<<<< HEAD
-import Chatbot from "./components/Chatbot";
-=======
-// import Chatbot from "./components/Chatbot";
->>>>>>> d6b6e75052f8281d7444f50a685872d1a8ac4e54
-
 
 function App() {
   const [selectedProject, setSelectedProject] = useState(null);
